ba2eb58d631ca23b099e9adf0e37399f592dea91
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-07-10 02:21:43 +0530

    Second

--- contracts/RaffleStack.sol
+++ contracts/RaffleStack.sol
@@ -2,22 +2,40 @@
 pragma solidity^0.8;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 error RAFFLESTACK__NOTENOUGHETHTOENTER();
 
 contract RaffleStack is VRFConsumerBaseV2 {
     // State variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
-    mapping (address => uint) addressToAmountFunded;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // Lottery winners
+    address private s_recentWinner;
 
     // Events
     event RaffleStackEntered(address indexed player);
+    event RequestedRaffleWinner(uint256 requestId);
 
-    
-
-
-    constructor(uint256 entranceFee, address vrfCoordinator) VRFConsumerBaseV2(vrfCoordinator){
+    constructor(
+        uint256 entranceFee, 
+        address vrfCoordinator, 
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator){
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffleStack() public payable{
@@ -33,13 +51,23 @@ contract RaffleStack is VRFConsumerBaseV2 {
         // 1. Request a random number using VRF
         // Chainlink VRF is actually a two step process. First we have to request a random number then we'll recieve the random winner.
         // 2. Then declare the random selected address as winner
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, //Gaslane -  The max amount of link that we are will to pay for a random number
+            i_subscriptionId, // Is the needed to pay for the Oracle gas (Link)
+            REQUEST_CONFIRMATIONS, // How many blocks should a Chainlink node should wait before responding
+            i_callbackGasLimit, // Protects us from requesting random no. when our code becomes gas intensive
+            NUM_WORDS // Number of random numbers we want to get
+        );
+        emit RequestedRaffleWinner(requestId);
     }
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) 
         internal 
         override 
     {
-
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
     }
 
     // View/Pure functions
@@ -48,6 +76,9 @@ contract RaffleStack is VRFConsumerBaseV2 {
     }
     function getPlayer(uint256 index) public view returns(address){
         return s_players[index];
+    }
+    function getRecentWinner() public view returns(address){
+        return s_recentWinner;
     }
 
     
