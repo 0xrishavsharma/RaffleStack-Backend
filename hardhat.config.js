f8552104826622fa45ccaa656b655ee6e731e18c
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-07-15 02:06:29 +0530

    Ninth

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/LvnRUn7s2ipfmZJ55mIHNO2Oa9uTTGpZ";
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
